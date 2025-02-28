e75efcb598ba0265c13b598a7fe94427fabba028
Jorge Octavio Guzmán Sánchez <jogs78@gmail.com>
2025-02-27 19:03:40 -0600

    Mostar funcionamiento de rutas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/saludar",function (){
+	echo "hola";
+});
+
+Route::get('/usuarios/agregar',function (){
+    return view('formulario');
+});
+
+Route::get('/usuarios/listar',function (){
+    return view('lista');
+});
+
+Route::get('/usuarios/listado',function (){
+    return view('listado');
+});
